891df893ba6fef3ff154da3d358bd4e86e29c4b5
scottsc0de <scott5902@gmail.com>
2023-01-22 06:18:04 -0500

    motm vs mmlp

--- public/app.js
+++ public/app.js
@@ -71,7 +71,7 @@ btnOne.addEventListener('click', function (e) {
     updateLiveScore('scoreOne');
 
     canVote = false;
-    localStorage.setItem('ice', 'cream');
+    localStorage.setItem('oh', 'okay');
 });
 
 btnTwo.addEventListener('click', function (e) {
@@ -81,13 +81,13 @@ btnTwo.addEventListener('click', function (e) {
     updateLiveScore('scoreTwo');
 
     canVote = false;
-    localStorage.setItem('ice', 'cream');
+    localStorage.setItem('oh', 'okay');
 });
 
 window.addEventListener('load', function (e) {
     displayLiveScore();
     // clearStorage();
-    if (localStorage.getItem('ice', 'cream')) {
+    if (localStorage.getItem('oh', 'okay')) {
         voted();
     }
 });
