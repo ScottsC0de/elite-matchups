7d3198d50ebcbd8b7c398cb1bf8953707edb5cd6
scottsc0de <scott5902@gmail.com>
2023-01-24 05:28:16 -0500

    cali vs btw

--- public/app.js
+++ public/app.js
@@ -71,7 +71,7 @@ btnOne.addEventListener('click', function (e) {
     updateLiveScore('scoreOne');
 
     canVote = false;
-    localStorage.setItem('fried', 'dumplings');
+    localStorage.setItem('peace', 'love');
 });
 
 btnTwo.addEventListener('click', function (e) {
@@ -81,13 +81,13 @@ btnTwo.addEventListener('click', function (e) {
     updateLiveScore('scoreTwo');
 
     canVote = false;
-    localStorage.setItem('fried', 'dumplings');
+    localStorage.setItem('peace', 'love');
 });
 
 window.addEventListener('load', function (e) {
     displayLiveScore();
     // clearStorage();
-    if (localStorage.getItem('fried', 'dumplings')) {
+    if (localStorage.getItem('peace', 'love')) {
         voted();
     }
 });
