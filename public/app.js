f7453c5da4b7660d8da1e3847b045f0abc7829a4
scottsc0de <scott5902@gmail.com>
2023-01-27 05:16:52 -0500

    blink

--- public/app.js
+++ public/app.js
@@ -1,11 +1,14 @@
 var btnOne = document.getElementById('btn-one');
 var btnTwo = document.getElementById('btn-two');
+var btnThree = document.getElementById('btn-three');
 var scoreOne = document.getElementById('score-one');
 var scoreTwo = document.getElementById('score-two');
+var scoreThree = document.getElementById('score-three');
 // var clearBtn = document.getElementById('clear-btn');
 
 let scoreboardOne = 0;
 let scoreboardTwo = 0;
+let scoreboardThree = 0;
 
 // var canVote = true;
 
@@ -26,9 +29,11 @@ const displayLiveScore = () => {
 
             scoreOne.textContent = data.scoreOne;
             scoreTwo.textContent = data.scoreTwo;
+            scoreThree.textContent = data.scoreThree;
 
             scoreboardOne = data.scoreOne;
             scoreboardTwo = data.scoreTwo;
+            scoreboardThree = data.scoreThree;
         });
 };
 
@@ -49,6 +54,7 @@ const updateLiveScore = (score) => {
 const voted = () => {
     btnOne.disabled = true;
     btnTwo.disabled = true;
+    btnThree.disabled = true;
 };
 
 // add 1 score to content 1
@@ -63,6 +69,11 @@ const scoreTwoFunction = () => {
     scoreTwo.textContent = scoreboardTwo;
 };
 
+const scoreThreeFunction = () => {
+    scoreboardThree += 1;
+    scoreThree.textContent = scoreboardThree;
+};
+
 // onclick disable buttons, add 1 to score
 btnOne.addEventListener('click', function (e) {
     // e.preventDefault();
@@ -71,7 +82,7 @@ btnOne.addEventListener('click', function (e) {
     updateLiveScore('scoreOne');
 
     canVote = false;
-    localStorage.setItem('8675', '309');
+    localStorage.setItem('omg', 'yay');
 });
 
 btnTwo.addEventListener('click', function (e) {
@@ -81,13 +92,23 @@ btnTwo.addEventListener('click', function (e) {
     updateLiveScore('scoreTwo');
 
     canVote = false;
-    localStorage.setItem('8675', '309');
+    localStorage.setItem('omg', 'yay');
+});
+
+btnThree.addEventListener('click', function (e) {
+    // e.preventDefault();
+    voted();
+    scoreThreeFunction();
+    updateLiveScore('scoreThree');
+
+    canVote = false;
+    localStorage.setItem('omg', 'yay');
 });
 
 window.addEventListener('load', function (e) {
     displayLiveScore();
     // clearStorage();
-    if (localStorage.getItem('8675', '309')) {
+    if (localStorage.getItem('omg', 'yay')) {
         voted();
     }
 });
