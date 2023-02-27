902a41f9a117d38afc1e65c762a8e2a0dae2afb8
scottsc0de <scott5902@gmail.com>
2023-02-27 04:58:56 -0500

    70sv90s

--- public/app.js
+++ public/app.js
@@ -87,21 +87,21 @@ btnOne.addEventListener('click', function (e) {
     voted();
     scoreOneFunction();
     updateLiveScore('scoreOne');
-    localStorage.setItem('thenewyorkkyankeees', 'andtheneyorkmets');
+    localStorage.setItem('doesanynonenha', 'veaphonecharrger');
 });
 
 btnTwo.addEventListener('click', function (e) {
     voted();
     scoreTwoFunction();
     updateLiveScore('scoreTwo');
-    localStorage.setItem('thenewyorkkyankeees', 'andtheneyorkmets');
+    localStorage.setItem('doesanynonenha', 'veaphonecharrger');
 });
 
 // btnThree.addEventListener('click', function (e) {
 //     voted();
 //     scoreThreeFunction();
 //     updateLiveScore('scoreThree');
-//     localStorage.setItem('thenewyorkkyankeees', 'andtheneyorkmets');
+//     localStorage.setItem('doesanynonenha', 'veaphonecharrger');
 // });
 
 // btnFour.addEventListener('click', function (e) {
@@ -114,7 +114,7 @@ btnTwo.addEventListener('click', function (e) {
 window.addEventListener('load', function (e) {
     displayLiveScore();
 
-    if (localStorage.getItem('thenewyorkkyankeees', 'andtheneyorkmets')) {
+    if (localStorage.getItem('doesanynonenha', 'veaphonecharrger')) {
         voted();
     }
 });
